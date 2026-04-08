24bb9a41f5eb312e2aaae7235776a4c959e48f75
ocarina0001 <117322863+ocarina0001@users.noreply.github.com>
2026-04-08 11:16:04 -0400

    survivor xenotype

---
+++ 1.6/Source/RainWorld/PawnRenderNode_FurUseSkinColor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace RainWorld
+{
+	public class PawnRenderNode_FurUseSkinColor : PawnRenderNode
+	{
+		protected override Shader DefaultShader => ShaderDatabase.CutoutSkinOverlay;
+		public PawnRenderNode_FurUseSkinColor(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) : base(pawn, props, tree) { }
+		public override Graphic GraphicFor(Pawn pawn)
+		{
+			if (!ModLister.CheckBiotech("Fur"))
+			{
+				return null;
+			}
+			if (pawn.story?.furDef == null)
+			{
+				return null;
+			}
+			return GraphicDatabase.Get<Graphic_Multi>(pawn.story?.furDef.GetFurBodyGraphicPath(pawn), ShaderFor(pawn), Vector2.one, ColorFor(pawn));
+		}
+		public override Color ColorFor(Pawn pawn)
+		{
+			return pawn.story.SkinColor;
+		}
+	}
+}
