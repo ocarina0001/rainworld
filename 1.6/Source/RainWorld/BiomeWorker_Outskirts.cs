d8e21dc1872310bb3244b13fb8bc50bec493f77e
ocarina0001 <117322863+ocarina0001@users.noreply.github.com>
2026-04-16 13:24:45 -0400

    fuckin around with biome stuff

--- 1.6/Source/RainWorld/BiomeWorker_Outskirts.cs
+++ 1.6/Source/RainWorld/BiomeWorker_Outskirts.cs
@@ -5,108 +5,30 @@ using UnityEngine;
 using Verse;
 using Verse.Noise;
 
-public class BiomeWorker_Outskirts : BiomeWorker
+namespace RainWorld
 {
-    private Perlin perlin;
-
-    private static readonly List<PlanetTile> NeighborList = new List<PlanetTile>();
-
-    private const float ScarlandsThreshold = 0.9f;
-    private const int SeedPart = 417235213;
-
-    public override float GetScore(BiomeDef biome, Tile tile, PlanetTile planetTile)
+    public class BiomeWorker_Outskirts : BiomeWorker
     {
-        if (tile.WaterCovered)
+        private const float IdealTemperature = 12f;
+        private const float IdealRainfall = 1000f;
+        private const float TemperatureTolerance = 3f;
+        private const float RainfallTolerance = 150f;
+        private const float MaxScore = 30f;
+
+        public override float GetScore(BiomeDef biome, Tile tile, PlanetTile planetTile)
         {
-            return -100f;
+            if (tile.WaterCovered)
+                return -100f;
+            if (tile.temperature < -5f || tile.temperature > 30f)
+                return 0f;
+            if (tile.rainfall < 400f || tile.rainfall > 2000f)
+                return 0f;
+            float tempDiff = tile.temperature - IdealTemperature;
+            float rainDiff = tile.rainfall - IdealRainfall;
+            float tempFactor = Mathf.Exp(-0.5f * (tempDiff * tempDiff) / (TemperatureTolerance * TemperatureTolerance));
+            float rainFactor = Mathf.Exp(-0.5f * (rainDiff * rainDiff) / (RainfallTolerance * RainfallTolerance));
+            float score = MaxScore * tempFactor * rainFactor;
+            return Mathf.Max(0f, score);
         }
-
-        if (!AllowedAt(planetTile))
-        {
-            return 0f;
-        }
-
-        return 50f;
-    }
-
-    private bool AllowedAt(PlanetTile tile)
-    {
-        if (IsScarlandsEligible(tile))
-        {
-            return false;
-        }
-
-        Find.WorldGrid.GetTileNeighbors(tile, NeighborList);
-        if (NeighborList.Empty())
-        {
-            return false;
-        }
-
-        foreach (PlanetTile n in NeighborList)
-        {
-            if (IsScarlandsEligible(n))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private bool IsScarlandsEligible(PlanetTile tile)
-    {
-        Tile t = Find.WorldGrid[tile];
-
-        if (t.WaterCovered)
-        {
-            return false;
-        }
-
-        if (!BiomeWorker_TemperateForest.Allowed(t))
-        {
-            return false;
-        }
-
-        float n = NoiseAt(tile);
-        if (n < ScarlandsThreshold)
-        {
-            return false;
-        }
-
-        Find.WorldGrid.GetTileNeighbors(tile, NeighborList);
-        if (NeighborList.Empty())
-        {
-            return false;
-        }
-
-        foreach (PlanetTile neighbor in NeighborList)
-        {
-            if (NoiseAt(neighbor) >= ScarlandsThreshold)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private float NoiseAt(PlanetTile tile)
-    {
-        if (perlin == null)
-        {
-            perlin = new Perlin(
-                0.2,
-                0.5,
-                0.1,
-                4,
-                true,
-                false,
-                Gen.HashCombineInt(Find.World.info.Seed, SeedPart),
-                QualityMode.Medium
-            );
-        }
-
-        Vector3 c = Find.WorldGrid.GetTileCenter(tile);
-        return (float)perlin.GetValue(c.x, c.y, c.z);
     }
 }
